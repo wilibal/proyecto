218f0577ac784b0389a70cf063955ed1e430cd78
wilibal <52547386+wilibal@users.noreply.github.com>
2019-07-04 14:48:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+//var opciones = ["piedra", "papel", "tijera"];
+var opciones = [0, 1, 2];
+var eleccionMaquina;
+
+
+function aleatorio(minimo, maximo){
+    var numero = Math.floor(Math.random() * (maximo - minimo +1) + minimo);
+    return numero;
+}
+
+function usuario(eleccionUsuario){
+    eleccionUsuario = parseInt(eleccionUsuario);
+    eleccionMaquina = aleatorio(0,2);
+    //alert(eleccionUsuario);
+    //alert(eleccionMaquina);
+
+    if(eleccionUsuario == 0){//el usuario eligio piedra 
+        if(opciones[eleccionMaquina] == 1){//si la maquina eligio papel 
+            document.getElementById('efecto').innerHTML ='<h1>¡Perdiste!</h1> <h3>Sack eligio papel y tu piedra.</h3>';
+        }else{
+            if(opciones[eleccionMaquina] == 2){
+                document.getElementById('efecto').innerHTML ='<h1>¡Ganaste!</h1> <h3>Sack eligio tijera y tu piedra.</h3>';
+            }else{
+                if(opciones[eleccionMaquina] == 0){
+                    document.getElementById('efecto').innerHTML ='<h1>¡Empate!</h1> <h3>Ambos eligieron piedra.</h3>';
+                }
+            }
+        } 
+    }
+
+    if(eleccionUsuario == 1){//el usuario eligio papel 
+        if(opciones[eleccionMaquina] == 2){
+            document.getElementById('efecto').innerHTML ='<h1>¡Perdiste!</h1> <h3>Sack eligio tijera y tu papel.</h3>';
+        }else{
+            if(opciones[eleccionMaquina] == 0){
+                document.getElementById('efecto').innerHTML ='<h1>¡Ganaste!</h1> <h3>Sack eligio piedra y tu papel.</h3>';
+                
+            }else{
+                if(opciones[eleccionMaquina] == 1){
+                    document.getElementById('efecto').innerHTML ='<h1>¡Empate!</h1> <h3>Ambos eligieron papel.</h3>'; 
+                }
+            }
+        }
+    }
+
+    if(eleccionUsuario == 2) {//el usuario eligio tijera 
+        if(opciones[eleccionMaquina] == 1){
+            document.getElementById('efecto').innerHTML ='<h1>¡Ganaste!</h1> <h3>Sack eligio papel y tu tijera.</h3>';
+        
+        }else{
+            if(opciones[eleccionMaquina] == 0){
+                document.getElementById('efecto').innerHTML ='<h1>¡Perdiste!</h1> <h3>Sack eligio piedra y tu tijera.</h3>'; 
+            }else{
+                if(opciones[eleccionMaquina] == 2) {
+                    document.getElementById('efecto').innerHTML ='<h1>¡Empate!</h1> <h3>Sack eligieron tijera.</h3>';
+                }
+            }
+        }
+    }
+
+    document.getElementById('efecto').style.display = "";
+}
+
+
+
+function quitarEfecto() {
+    document.getElementById('efecto').style.display = "none";
+}
